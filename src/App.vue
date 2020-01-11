0376c957fb18a21feae1fa051ff12569e7936fa6
mixereqv1 <mateuszral7@gmail.com>
2020-01-11 16:07:53 +0100

    Add item component with styles and add ad image

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,9 @@
 <template>
-  <div class="app">
-    <Search />
+  <div :class="[{ flexStart: step === 1 }, 'app']">
+    <Search v-model="searchValue" @input="handleInput" />
+    <div class="results" v-if="step === 1 && !loading && results">
+      <Item v-for="item in results" :item="item" :key="item.id" />
+    </div>
   </div>
 </template>
 
@@ -8,50 +11,76 @@
 import axios from 'axios';
 import debounce from 'debounce';
 import Search from './components/Search.vue';
+import Item from './components/Item.vue';
 
 const baseURL = 'https://api.themoviedb.org/3/search/movie?api_key=4076f33084bc57273f90e8fcf48bff6e&language=pl&query=';
-console.log(baseURL);
 
 export default {
   name: 'App',
   components: {
     Search,
+    Item,
 
   },
   data() {
     return {
       searchValue: '',
       step: 0,
+      loading: false,
       results: [],
     };
   },
   methods: {
     // eslint-disable-next-line
     handleInput: debounce(function() {
+      this.loading = true;
       axios.get(`${baseURL}${this.searchValue}`)
         .then((response) => {
-          console.log(response);
+          if (response.status === 200) {
+            this.loading = false;
+            this.step = 1;
+
+            const { data } = response;
+            this.results = data.results;
+            console.log(data);
+          }
         });
-    }, 500),
+    }, 600),
   },
 };
 </script>
 
 <style lang="scss" scoped>
+  @import url('https://fonts.googleapis.com/css?family=Montserrat:300,400,600,800&display=swap');
   *,
   *::before,
   *::after {
     margin: 0;
     padding: 0;
     box-sizing: border-box;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    font-family: 'Montserrat', sans-serif;
   }
   .app {
     width: 100%;
-    height: 100vh;
-    background: crimson;
+    min-height: 100vh;
+    background: gray;
     display: flex;
     flex-direction: column;
     justify-content: center;
     align-items: center;
+
+    &.flexStart {
+      justify-content: flex-start;
+    }
+
+    .results {
+      width: 100%;
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+      margin-top: 20px;
+    }
   }
 </style>
---
+++ src/components/Item.vue
@@ -0,0 +1,122 @@
+<template>
+  <div class="item">
+      <span class="releaseDate"> {{ releaseDate }} </span>
+      <span class="forAdult" v-if="forAdult">+18</span>
+      <span class="voteAverage"> Ocena: {{ voteAverage }} </span>
+      <span :style="style" class="poster"></span>
+      <div class="movie">
+        <h1> {{ title }} </h1>
+        <p> {{ description }} </p>
+        <a target="_blank" class="movieLink" :href="movie">TMDb</a>
+      </div>
+  </div>
+</template>
+
+<script>
+const posterURL = 'https://image.tmdb.org/t/p/w185_and_h278_bestv2';
+const movieURL = 'https://www.themoviedb.org/movie/';
+
+export default {
+  name: 'Item',
+  props: {
+    item: {
+      type: Object,
+      required: true,
+    },
+  },
+  data() {
+    return {
+      poster: `${posterURL}${this.item.poster_path}`,
+      title: this.item.title,
+      description: this.item.overview,
+      releaseDate: this.item.release_date,
+      voteAverage: this.item.vote_average,
+      movie: `${movieURL}${this.item.id}`,
+      forAdult: this.item.adult,
+    };
+  },
+  computed: {
+    style() {
+      return `background-image: url('${this.poster}')`;
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+    .item {
+      width: 90%;
+      height: auto;
+      min-height: auto;
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+      background: #fff;
+      border-radius: 15px;
+      padding: 20px;
+      margin: 10px;
+      text-align: center;
+      position: relative;
+
+      .releaseDate,
+      .voteAverage,
+      .forAdult {
+        position: absolute;
+        top: 10px;
+        right: 10px;
+        font-weight: 600;
+        font-size: 1.1em;
+      }
+
+      .voteAverage {
+        left: 10px;
+        right: auto;
+
+        @media (min-width: 768px) {
+          top: auto;
+          left: auto;
+          bottom: 10px;
+          right: 10px;
+        }
+      }
+
+      .forAdult {
+        top: 30px;
+        color: #850808;
+      }
+
+      @media (min-width: 768px) {
+        width: 80%;
+        flex-direction: row;
+      }
+
+      .poster {
+        background-size: contain;
+        background-repeat: no-repeat;
+        width: 185px;
+        height: 278px;
+      }
+
+      .movie {
+        width: 100%;
+        height: 60%;
+
+        @media (min-width: 768px) {
+          height: 100%;
+          width: 70%;
+          margin-left: 10px;
+        }
+
+        .movieLink {
+          // display: inline-flex;
+          // width: 100%;
+          text-align: left;
+          font-size: 1.2em;
+          font-weight: 600;
+          text-decoration: none;
+          color: #1a1a1a;
+          cursor: pointer;
+        }
+      }
+    }
+</style>
